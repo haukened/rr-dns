7b13d17106dcd2c9f477b7b499da3decc8283e5c
David Haukeness <david@hauken.us>
2025-07-30 15:18:12 +0000

    add resolver structure

---
+++ internal/dns/services/resolver/interfaces.go
@@ -0,0 +1,15 @@
+package resolver
+
+import "github.com/haukened/rr-dns/internal/dns/domain"
+
+type DNSCodec interface {
+	// Upstream Functions
+	// These methods are used to encode and decode DNS messages for communication with upstream servers.
+	EncodeQuery(query domain.DNSQuery) ([]byte, error)
+	DecodeResponse(data []byte, expectedID uint16) (domain.DNSResponse, error)
+
+	// Authoritative Functions
+	// These methods handle encoding and decoding of authoritative records for zone file management.
+	DecodeQuery(data []byte) (domain.DNSQuery, error)
+	EncodeResponse(resp domain.DNSResponse) ([]byte, error)
+}
